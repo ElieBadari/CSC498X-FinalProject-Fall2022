0b5e9b46c0446f85769bb58df535457addee9ea1
ElieBadari <elie.badari@hotmail.com>
2022-12-15 15:08:07 +0200

    added landing page

--- linknet-app/src/app/app-routing.module.ts
+++ linknet-app/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
   }
 ];
 @NgModule({
